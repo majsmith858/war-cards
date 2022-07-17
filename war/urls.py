7c5fceb9513ef12dfca73d58a11447df52173dab
msmith <majae.smith@integrity.gov.jm>
2022-07-17 01:26:04 -0500

    Get the winner and reset scores - Created get_winner, clear_score, updated play_game functionality
    - Created clear score url in urls.py

--- war/urls.py
+++ war/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from . import views
-from war.views import index, generate_deck, play_game, calculate_score
+from war.views import index, generate_deck, play_game, calculate_score, clear_score
 
 urlpatterns = [
     path('', index, name='index'),
     path('shuffle-deck/', generate_deck, name='shuffle-deck'),
     path('play/<str:player_1_deck>/<str:player_2_deck>/', play_game, name='play-game'),
     path('calculate-score/<str:player_1_deck>/<str:player_2_deck>/<str:player_1_card>/<str:player_2_card>/', calculate_score, name='cal-score'),
+    path('clear-score/', clear_score, name='clear-score')
 ]
--- war/views.py
+++ war/views.py
@@ -66,3 +66,59 @@ def calculate_score(request,player_1_deck, player_2_deck, player_1_card, player_
         save_score(identity)
         
     return HttpResponseRedirect(reverse("play-game", args=[player_1_deck,player_2_deck]))
+    
+def get_winner(players,score=0):
+    """
+    This function gets the winner of the game
+    
+    :param players: a list of player objects
+    :param score: the score that the players are trying to reach, defaults to 0 (optional)
+    """
+    for player in players:
+        if score == 0:
+            score = player.score
+            winner = {'Winner':player.identity, 'Score':player.score}
+        
+        if player.score > score:
+            score = player.score
+            winner ={'Winner':player.identity, 'Score':player.score}
+    return winner
+
+def play_game(request, player_1_deck, player_2_deck):
+    """
+    It takes in a request and two decks of cards and plays a game of war.
+    
+    :param request: the request object
+    :param player_1_deck: A list of card objects that represent the first player's deck
+    :param player_2_deck: A list of card objects that represent the second player's deck
+    """
+    template_name='war/play.html'
+    game_over=False
+    players = Player.objects.all()
+        
+    player_1_draw = draw_card(player_1_deck)
+    player_2_draw = draw_card(player_2_deck)
+        
+    if not (player_1_draw or player_1_draw):
+        game_over=True
+        winner = get_winner(players)
+
+        context = {'winner':winner, 'game_over':game_over}
+    else:
+        context = {"players":players, "player_1_draw":player_1_draw[0],
+                "player_2_draw":player_2_draw[0],'player_1_deck':player_1_deck,
+                'player_2_deck':player_2_deck, 'game_over':game_over}  
+        
+    return render(request, template_name, context=context)
+
+def clear_score(request):
+    """
+    It clears the score of the players when the game is over.
+    
+    :param request: The request object
+    """
+    players = Player.objects.all()
+    for player in players:
+        player.score = 0
+        player.save()
+    return HttpResponseRedirect(reverse("shuffle-deck"))
